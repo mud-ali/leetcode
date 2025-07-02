e94f10c3e04b3cd4f394281b2f7fd121e3908e07
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-07-02 19:47:46 -0400

    1768

---
+++ 1768-merge-strings-alternately/main.ts
@@ -0,0 +1,27 @@
+function mergeAlternately(word1: string, word2: string): string {
+    let output = "";
+    let i1 = 0, i2 = 0;
+    for (let i = 0; i < word1.length + word2.length; i++) {
+        if (i % 2 === 0) {
+            if (i1 < word1.length) {
+                output += word1.charAt(i1++);
+                continue;
+            }
+        } else if (i2 < word2.length) {
+            output += word2.charAt(i2++);
+            continue;
+        }
+        if (i1 >= word1.length) {
+            output += word2.substring(i2);
+            break;
+        }
+        if (i2 >= word2.length) {
+            output += word1.substring(i1);
+            break;
+        }
+    }
+    return output;
+}
+
+console.log("jello")
+console.log(mergeAlternately("abc", "pqr"));
