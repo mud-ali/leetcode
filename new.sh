90b91c5d0cbae5c16686c0679eb723a8d0a44233
mudasir <96320211+mud-ali@users.noreply.github.com>
2025-07-03 19:53:22 -0400

    stop auto copying build script

--- new.sh
+++ new.sh
@@ -7,6 +7,6 @@ mkdir "$sanitized_name"
 read "lang?Choose a language (js, ts, py, cpp, java, c): "
 cd "$sanitized_name"
 touch "main.$lang"
-cp ../templates/build.$lang.sh ./build.sh
-chmod +x build.sh
+# cp ../templates/build.$lang.sh ./build.sh
+# chmod +x build.sh
 echo "# $dir_name \n" > README.md
